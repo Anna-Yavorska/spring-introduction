cd6f9db039dd9d6b69135efe8829461d9650e203
Anna Yavorska <aniayavorsk@gmail.com>
2023-06-25 23:53:09 +0200

    hw_38

--- src/main/java/javahome/springbootstudent/hw_38/task_2/controller/PrintingProductsController.java
+++ src/main/java/javahome/springbootstudent/hw_38/task_2/controller/PrintingProductsController.java
@@ -1,51 +1,61 @@
-package javahome.springbootstudent.hw_38.task_2;
+package javahome.springbootstudent.hw_38.task_2.controller;
 
+import javahome.springbootstudent.hw_38.task_1.model.Product;
+import javahome.springbootstudent.hw_38.task_1.repository.ProductRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collection;
+
 @RestController
-@RequestMapping("/printing")
+@RequestMapping("/products")
 public class PrintingProductsController {
-    @GetMapping("/products")
+    private final ProductRepository productRepository;
+
+    public PrintingProductsController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/page")
     public String printProducts() {
-        return """
+        String header = """
                 <!DOCTYPE html>
                 <html lang="en">
                   <head>
                     <meta charset="utf-8">
                     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                    <title>Заголовок страницы</title>
-                    <link rel="stylesheet" href="./styles/style.css">
-                                
-                    <meta property="og:title" content="Заголовок страницы в OG">
-                    <meta property="og:description" content="Описание страницы в OG">
-                    <meta property="og:image" content="https://example.com/image.jpg">
-                    <meta property="og:url" content="https://example.com/">
+                    <title>Products</title>
                   </head>
                   <body>
                     <header>
                       <h1>Products</h1>
                     </header>
-                    <main>
+                """;
+
+        String main = """
+                <main>
                         <section>
                           <h2>Products list of "Lidl" shop</h2>
                           <ul>
-                          <li>"id":1,"name":"Potato","description":"3kg in pack","price":3.50</li>
-                          <li>"id":2,"name":"Orange","description":"From Turkey","price":6.9</li>
-                          <li>"id":3,"name":"Water","description":"Highly carbonated","price":2.0</li>
-                          <li>"id":4,"name":"Corn","description":"Canned corn 2 in pack","price":7.15</li>
-                          <li>"id":5,"name":"Cherry","description":"1kg in pack","price":20.0</li>
-                          <li>"id":6,"name":"Bread","description":"Toast bread","price":1.9</li>
-                          <li>"id":7,"name":"Chocolate","description":"Dark chocolate 100g","price":7.45</li>                          
-                        </ul>
-                        </section>
-                    </main>
+                """;
+        Collection<Product> allProducts = productRepository.getAllProducts();
+        for (Product product : allProducts) {
+            main = main.concat("<li>" + product + "</li>");
+        }
+        main = main.concat("""
+                </ul>
+                </section>
+                </main>
+                """);
+
+        String footer = """
                     <footer>
                       <p>If you would like to become our supplier, please contact our <a href="https://www.google.com/" target="_blank" rel="noopener no-referrer">admin</a></p>
                     </footer>
                   </body>
                 </html>
                 """;
+        return header + main + footer;
     }
 }
