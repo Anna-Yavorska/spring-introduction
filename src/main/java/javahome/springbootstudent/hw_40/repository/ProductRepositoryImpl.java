40970ed775c7f4140591dca17e8c7ca62c3790a5
Pavel Radkevich <pradkevich@eisgroup.com>
2023-07-02 13:46:07 +0300

    Поправил условие поиска

--- src/main/java/javahome/springbootstudent/hw_40/repository/ProductRepositoryImpl.java
+++ src/main/java/javahome/springbootstudent/hw_40/repository/ProductRepositoryImpl.java
@@ -77,15 +77,16 @@ public class ProductRepositoryImpl implements ProductRepository {
         } catch (SQLException e) {
             e.printStackTrace();
         }
-        return null;
+        return productToCreate.id();
     }
 
     @Override
     public List<Product> searchByDescription(String description) {
         List<Product> products = new ArrayList<>();
         try (Connection connection = dataSource.getConnection()) {
-            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM products WHERE description LIKE '%?' ");
-            preparedStatement.setString(1, description);
+            String condition = "%" + description + "%";
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT * FROM products WHERE description LIKE ? ");
+            preparedStatement.setString(1, condition);
             ResultSet rs = preparedStatement.executeQuery();
             while (rs.next()) {
                 Product product = new Product(rs.getInt(1), rs.getString(2), rs.getString(3), rs.getDouble(4));
